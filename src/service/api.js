5b0467317ae354d0ed294904f39d53d6d3618eb3
majid-abdalla <shoble33@hotmail.com>
2026-07-19 21:28:55 +0300

    fix: remove duplicate /api from baseURL

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   //baseURL:'http://localhost:8080/api',
   //baseURL: 'http://localhost:5129/api',
-  baseURL: import.meta.env.VITE_API_URL + '/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
